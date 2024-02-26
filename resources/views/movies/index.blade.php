ab9a57fad991cba11d614a0e7dce3e9cf4a02c60
MirkoCifelli <ciffo4@hotmail.it>
2024-02-26 15:42:57 +0100

    Change style scss

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,35 +3,35 @@
 @section('page-title', 'Tutti i Film')
 
 @section('main-content')
-<h1 class="text-center">
-    Film
-</h1>
+    <h1 class="text-center">
+        Film
+    </h1>
 
-<div class="row justify-content-center ">
-    @foreach ($movies as $index => $movie)
-        <div class="col-3 m-3 ">
-            <div class="card">
-                <div class="card-body">
-                    <h3 class="text-center">
-                        {{ $movie->title }}
-                    </h3>
-                    <ul class=" list-unstyled text-center">
-                        <li>
-                           Titolo Originale: {{$movie->original_title}}
-                        </li>
-                        <li>
-                          Nazionalita: {{$movie->nationality}}
-                        </li>
-                        <li>
-                          Data: {{$movie->date}}
-                        </li>
-                        <li>
-                           Voto: {{$movie->vote}}
-                        </li>
-                    </ul>
+    <div class="row justify-content-center ">
+        @foreach ($movies as $index => $movie)
+            <div class="col-3 m-3 ">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="text-center">
+                            {{ $movie->title }}
+                        </h3>
+                        <ul class="list-unstyled">
+                            <li>
+                                Titolo Originale: {{ $movie->original_title }}
+                            </li>
+                            <li>
+                                Nazionalita: {{ $movie->nationality }}
+                            </li>
+                            <li>
+                                Data: {{ $movie->date }}
+                            </li>
+                            <li>
+                                Voto: {{ $movie->vote }}
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    @endforeach
-</div>
+        @endforeach
+    </div>
 @endsection
